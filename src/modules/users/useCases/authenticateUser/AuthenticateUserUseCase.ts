b72b42bb98b9d71f62de119ca36f36cf5ba729cd
Leandro <leandrofontellas@gmail.com>
2022-02-13 12:19:44 -0300

    feat(authenticate): :sparkles: user can be authenticated in sessions route

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,52 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: IUserRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const token = sign({}, "503a9786c58aa5bb1176cf0db1eddd71dc19eeeb", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "../../modules/cars/routes/categories.routes";
 import { specificationsRoutes } from "../../modules/cars/routes/specification.routes";
+import { authenticateRoutes } from "../../modules/users/routes/authenticate.routes";
 import { usersRoutes } from "../../modules/users/routes/users.routes";
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use("/sessions", authenticateRoutes);
 
 export { router };
